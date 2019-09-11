4b41a124207bc898254b3045e3289dee686fa71d
Iñigo Telleria <inigo@arima.eu>
2019-09-11 18:31:18 +0200

    Add an empty spring-boot app

---
+++ src/main/java/eu/arima/springbootjibdocker/SpringBootJibDockerApplication.java
@@ -0,0 +1,13 @@
+package eu.arima.springbootjibdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJibDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJibDockerApplication.class, args);
+	}
+
+}
